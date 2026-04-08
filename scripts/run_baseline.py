22f3ba6ec8d819c8d1f3afe4a0f00e753c8e5306
vanshikasoni22 <vanshikasoni468@gmail.com>
2026-04-08 16:32:06 +0530

    alpha and epsilion training improved

--- scripts/run_baseline.py
+++ scripts/run_baseline.py
@@ -56,12 +56,12 @@ def choose_action(state, epsilon):
 def train(env, episodes=500):
     # save_q_table()
 
-    alpha = 0.1
+    alpha = 0.2
     gamma = 0.95
 
     epsilon = 1.0
-    epsilon_min = 0.05
-    epsilon_decay = 0.995
+    epsilon_min = 0.01
+    epsilon_decay = 0.99
 
     for ep in range(episodes):
         state = env.reset()
